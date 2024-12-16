1d22a2f91cae1d81ce1374459f1ff8f3189610fe
Felix Wollmann <50966625+felixwollmann@users.noreply.github.com>
2024-12-16 17:08:36 +0100

    try out hot potato

---
+++ lib/hot_potato.dart
@@ -0,0 +1,59 @@
+import 'dart:async';
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:shake_plus/shake_plus.dart';
+import 'package:vibration/vibration.dart';
+
+class HotPotato extends StatefulWidget {
+  const HotPotato({super.key});
+
+  @override
+  State<HotPotato> createState() => _HotPotatoState();
+}
+
+class _HotPotatoState extends State<HotPotato> {
+  var isSetOff = false;
+  var hasVibrator = Vibration.hasVibrator();
+
+  void _restart() {
+    isSetOff = false;
+    Timer(const Duration(seconds: 3), () {
+      setState(() {
+        isSetOff = true;
+      });
+    });
+
+  }
+
+  void _handleShake() async {
+    // setState(() {
+    //   s = Random().nextInt(6) + 1;
+    // });
+    // if (await hasVibrator ?? false) {
+    //   Vibration.vibrate(duration: 200);
+    // }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _restart();
+    ShakeDetector.autoStart(onPhoneShake: _handleShake);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: isSetOff ? Colors.red[500] : Theme.of(context).colorScheme.inversePrimary,
+        title: const Text("Hot Potato"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _restart,
+        tooltip: 'Increment',
+        child: const Icon(Icons.restart_alt_outlined),
+      ),
+    );
+  }
+}
